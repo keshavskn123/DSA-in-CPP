0e6a73cb03a36170975b69f300a49a5cd4a8f156
keshavskn123 <124264264+keshavskn123@users.noreply.github.com>
2025-07-05 13:37:23 +0530

    Add files via upload

---
+++ Binary Trees 1/FindNodeWithMaxValue.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Node{         // User Defined TreeNode
+public:
+    // Parameters
+    int val;
+    Node* left;
+    Node* right;
+    // Constructor with parametrs
+    Node(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void displayTree(Node* root) {      // Using Recursion
+    if(root == NULL) return;        // Base Case
+    cout << root->val << " ";
+    displayTree(root->left);        // left call/ Recursion
+    displayTree(root->right);        // right call/ Recursion
+}
+
+int maxInTree(Node* root) {
+    if(root==NULL) return INT_MIN;
+    int lMax = maxInTree(root->left);
+    int rMax = maxInTree(root->right);
+    return max(root->val, max(lMax, rMax));
+}
+int main() {
+    Node* a = new Node(1);
+    Node* b = new Node(2);
+    Node* c = new Node(3);
+    Node* d = new Node(4);
+    Node* e = new Node(5);
+    Node* f = new Node(6);
+    Node* g = new Node(7);
+    
+    a->left = b;
+    a->right = c;
+
+    b->left = d;
+    b->right = e;
+
+    c->left = f;
+    c->right = g;
+    displayTree(a);     // 1 2 4 5 3 6 7 
+    cout << endl;
+    cout << maxInTree(a);
+}
